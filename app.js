6379453611aac22a831c27c8ccfe6a660f3577dd
addaskogberg <adda.skogberg@gmail.com>
2018-03-09 10:17:09 +0100

    nodemon

--- app.js
+++ app.js
@@ -3,3 +3,10 @@
  * @author Adda Skogberg
  * @version 1.0
  */
+const express = require('express')
+const app = express()
+app.set('port', process.env.PORT || 5000)
+
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' + app.get('port') + ' ; press ctrl-c to terminate')
+})
