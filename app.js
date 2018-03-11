8d37b3b5f36c6450856e16479b6558864292a7f1
addaskogberg <adda.skogberg@gmail.com>
2018-03-11 19:43:31 +0100

    production

--- app.js
+++ app.js
@@ -5,7 +5,13 @@
  */
 const express = require('express')
 const app = express()
-app.set('port', process.env.PORT || 5000)
+var path = require('path')
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.set('port', process.env.PORT || 443)
 
 app.listen(app.get('port'), function () {
   console.log('Express started on http://localhost:' + app.get('port') + ' ; press ctrl-c to terminate')
